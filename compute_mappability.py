1d8b95599be9314140c0ccb46cbb62f3bec04420
Lachlan <lachlan.d@gmail.com>
2017-03-19 20:01:37 +1100

    Task 1

--- compute_mappability.py
+++ compute_mappability.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 
+# Lachlan Dryburgh 188607
+# 19/03/2017
+# COMP90016 - Assignment 1
+
+
 from Bio import SeqIO
 import sys
 
@@ -25,6 +30,7 @@ class mappability:
     return kmers
 
   def calc_mappability(self):
+    maps = [0.0]*(len(self.seq)-self.k+1)
     for i in range(len(self.seq)-self.k+1):
       if self.seq[i:i+self.k] in self.dic:
         n = len(self.dic[self.seq[i:i+self.k]])
@@ -32,7 +38,8 @@ class mappability:
         n = len(self.dic[self.seq[i:i+self.k].reverse_complement()])
       else:
         print("error at position " + str(i))
-      print(str(1.0/n))
+      maps[i] = 1.0/n
+    return maps 
 
 
 if (len(sys.argv) < 3):
@@ -42,8 +49,14 @@ if (len(sys.argv) < 3):
 try:
   for read in SeqIO.parse(sys.argv[1], "fasta"):
     mapper = mappability(read, int(sys.argv[2]))
-    mapper.calc_mappability()
+    maps = mapper.calc_mappability()
     break
 except IOError as e:
   print(e)
   sys.exit(1)
+
+f = open("map.wig", 'w')
+
+f.write("fixed step chrom=" + mapper.name + " start=1" + "\n")
+for m in maps:
+  f.write("{0:.2f}".format(m) + " ")
